1f3869e3ee9f477691b84e99a94f08a429c32f12
Neto test <bosgoty@mail.ru>
2024-02-04 22:18:11 +0700

    Initial commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,52 @@
+public class Radio {
+    public int currentVolume;
+    public int radioStation;
+
+    public void setRadioStation(int newRadioStation) {
+        if (newRadioStation < 10) {
+            if (newRadioStation >= 0) {
+                radioStation = newRadioStation;
+            }
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume <= 100) {
+            if (newCurrentVolume >= 0) {
+                currentVolume = newCurrentVolume;
+            }
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void next() {
+        if (radioStation < 9) {
+            radioStation = radioStation + 1;
+        } else {
+            if (radioStation == 9) {
+                radioStation = 0;
+            }
+        }
+    }
+
+    public void prev() {
+        if (radioStation > 0) {
+            radioStation = radioStation - 1;
+        } else {
+            if (radioStation == 0) {
+                radioStation = 9;
+            }
+        }
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,133 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class RadioTest {
+    @Test
+    void increaseVolumeMoreMax() { // повышаем звук выше верхней границы по звуку
+        Radio volume = new Radio();
+        volume.setCurrentVolume(100);
+        volume.increaseVolume();
+        int expected = 100;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void increaseVolumeValid() { // повышаем звук
+        Radio volume = new Radio();
+        volume.setCurrentVolume(50);
+        volume.increaseVolume();
+        int expected = 51;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void reduceVolumeLessMin() { // понижаем звук  ниже границы по звуку
+        Radio volume = new Radio();
+        volume.setCurrentVolume(0);
+        volume.reduceVolume();
+        int expected = 0;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void reduceVolumeValid() { // понижаем звук
+        Radio volume = new Radio();
+        volume.setCurrentVolume(50);
+        volume.reduceVolume();
+        int expected = 49;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextMoreMax() { // повышаем частоту выше верхней границы
+        Radio radio = new Radio();
+        radio.setRadioStation(9);
+        radio.next();
+        int expected = 0;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void nextValid() { // повышаем частоту
+        Radio radio = new Radio();
+        radio.setRadioStation(5);
+        radio.next();
+        int expected = 6;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void prevLessMin() { // понижаем частоту ниже нижней границы
+        Radio radio = new Radio();
+        radio.setRadioStation(0);
+        radio.prev();
+        int expected = 9;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void prevValid() { // понижаем частоту
+        Radio radio = new Radio();
+        radio.setRadioStation(5);
+        radio.prev();
+        int expected = 4;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setVolumeMoreMaxNoValid() { // невалидное значение звука выше границы
+        Radio volume = new Radio();
+        volume.setCurrentVolume(50);
+        volume.setCurrentVolume(101);
+        int expected = 50;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setVolumeLessMinNoValid() { // невалидное значение звука ниже границы
+        Radio volume = new Radio();
+        volume.setCurrentVolume(50);
+        volume.setCurrentVolume(-1);
+        int expected = 50;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setVolumeValid() { // выбрать валидное значение
+        Radio volume = new Radio();
+        volume.setCurrentVolume(50);
+        int expected = 50;
+        int actual = volume.currentVolume;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setRadioMinNoValid() { // невалидное значение частоты ниже границы
+        Radio radio = new Radio();
+        radio.setRadioStation(5);
+        radio.setRadioStation(-5);
+        int expected = 5;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setRadioMaxNoValid() { // невалидное значение частоты выше границы
+        Radio radio = new Radio();
+        radio.setRadioStation(5);
+        radio.setRadioStation(10);
+        int expected = 5;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void setRadioValid() { // выбрать валижное значение частоты
+        Radio radio = new Radio();
+        radio.setRadioStation(5);
+        int expected = 5;
+        int actual = radio.radioStation;
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
+
